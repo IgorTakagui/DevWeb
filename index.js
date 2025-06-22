364768e7d9098477ef05b1b820bd7707df07a96d
Igor Takagui Reis <igortakaguir@gmail.com>
2025-06-22 20:43:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,15 @@ const app = express();
 app.use(express.json());
 app.use('/produto', produtoRoutes);
 
-const DB_USER = 'SEU_USUARIO';
-const DB_PASSWORD = 'SUA_SENHA';
-const DB_CLUSTER = 'SEU_CLUSTER.mongodb.net';
-
 mongoose.connect(
     `mongodb+srv://HisgorMusical:takagui10@cluster0.aiedxys.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 )
   .then(() => {
+    const port = process.env.PORT || 4000;
     console.log('Conectado ao MongoDB Atlas!');
-    app.listen(3000);
+    app.listen(port, () => {
+        console.log(`Escutando na porta ${port}`);
+    });
   })
   .catch((err) => console.log(err));
